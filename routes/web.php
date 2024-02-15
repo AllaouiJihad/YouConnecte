0e0c7b7c0cd2549157a0f8e562066d87b7c73319
AllaouiJihad <allaouijihad05@gmail.com>
2024-02-15 14:38:10 +0100

    add middleware pour proteger le routre welcome

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('home');
-})->name('welcome');
+})->name('welcome')->middleware('auth');
 
 
 Route::get('/signup', [AuthController::class, 'showRegister'])->name('register');
