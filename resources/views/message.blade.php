27396ca535f77adc8c59a21ed4fa0ba556fba9cb
emminyell <140811695+emminyell@users.noreply.github.com>
2024-02-19 11:09:54 +0000

    ajouter message page

--- resources/views/message.blade.php
+++ resources/views/message.blade.php
@@ -1,361 +1,201 @@
+@extends('layouts/app')
+@section('content')
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+
+<script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
+<!------ Include the above in your HEAD tag ---------->
 <style>
-    // Video tutorial/codealong here: https://youtu.be/fCpw5i_2IYU
-//Github: https://github.com/FilipRastovic/whatsapp-chat-app-ui
-$blue: #74b9ff;
-
-body {
-  //background-image: url('https://images.unsplash.com/photo-1560568082-c15188aa6510?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=3300&q=80');
-  background-color: #3498db;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-rendering: optimizeLegibility;
-}
-
-.container {
-  margin: 60px auto;
-  background: #fff;
-  padding: 0;
-  border-radius: 7px;
-}
-
-.profile-image {
-  width: 50px;
-  height: 50px;
-  border-radius: 40px;
-}
-
-.settings-tray {
-  background: #eee;
-  padding: 10px 15px;
-  border-radius: 7px;
-
-  .no-gutters {
+    /*/////////////////////////////////*/
+/*/////////  chat styles  /////////*/
+/*/////////////////////////////////*/
+.chat
+{
+    list-style: none;
+    margin: 0;
     padding: 0;
-  }
+}
 
-  &--right {
+.chat li
+{
+    margin-bottom: 40px;
+    padding-bottom: 5px;
+    /* border-bottom: 1px dotted #B3A9A9; */
+    margin-top: 10px;
+    width: 80%;
+}
+
+
+.chat li .chat-body p
+{
+    margin: 0;
+    /* color: #777777; */
+}
+
+
+.chat-care
+{
+    overflow-y: scroll;
+    height: 350px;
+}
+.chat-care .chat-img
+{
+    width: 50px;
+    height: 50px;
+}
+.chat-care .img-circle
+{
+    border-radius: 50%;
+}
+.chat-care .chat-img
+{
+    display: inline-block;
+}
+.chat-care .chat-body
+{
+    display: inline-block;
+    max-width: 80%;
+    background-color: #FFC195;
+    border-radius: 12.5px;
+    padding: 15px;
+}
+.chat-care .chat-body strong
+{
+  color: #0169DA;
+}
+
+.chat-care .admin
+{
+    text-align: right ;
     float: right;
-
-    i {
-      margin-top: 10px;
-      font-size: 25px;
-      color: grey;
-      margin-left: 14px;
-      transition: .3s;
-
-      &:hover {
-        color: $blue;
-        cursor: pointer;
-      }
-    }
-  }
+}
+.chat-care .admin p
+{
+    text-align: left ;
+}
+.chat-care .agent
+{
+    text-align: left ;
+    float: left;
+}
+.chat-care .left
+{
+    float: left;
+}
+.chat-care .right
+{
+    float: right;
 }
 
-.search-box {
-  background: #fafafa;
-  padding: 10px 13px;
-
-  .input-wrapper {
-    background: #fff;
-    border-radius: 40px;
-
-    i {
-      color: grey;
-      margin-left: 7px;
-      vertical-align: middle;
-    }
-  }
-}
-
-input {
-  border: none;
-  border-radius: 30px;
-  width: 80%;
-
-  &::placeholder {
-    color: #e3e3e3;
-    font-weight: 300;
-    margin-left: 20px;
-  }
-
-  &:focus {
-    outline: none;
-  }
-}
-
-.friend-drawer {
-  padding: 10px 15px;
-  display: flex;
-  vertical-align: baseline;
-  background: #fff;
-  transition: .3s ease;
-
-  &--grey {
-    background: #eee;
-  }
-
-  .text {
-    margin-left: 12px;
-    width: 70%;
-
-    h6 {
-      margin-top: 6px;
-      margin-bottom: 0;
-    }
-
-    p {
-      margin: 0;
-    }
-  }
-
-  .time {
-    color: grey;
-  }
-
-  &--onhover:hover {
-    background: $blue;
-    cursor: pointer;
-
-    p,
-    h6,
-    .time {
-      color: #fff !important;
-    }
-  }
-}
-
-hr {
-  margin: 5px auto;
-  width: 60%;
-}
-
-.chat-bubble {
-  padding: 10px 14px;
-  background: #eee;
-  margin: 10px 30px;
-  border-radius: 9px;
-  position: relative;
-  animation: fadeIn 1s ease-in;
-
-  &:after {
-    content: '';
-    position: absolute;
-    top: 50%;
-    width: 0;
-    height: 0;
-    border: 20px solid transparent;
-    border-bottom: 0;
-    margin-top: -10px;
-  }
-
-  &--left {
-     &:after {
-      left: 0;
-      border-right-color: #eee;
-	    border-left: 0;
-      margin-left: -20px;
-    }
-  }
-
-  &--right {
-    &:after {
-      right: 0;
-      border-left-color: $blue;
-	    border-right: 0;
-      margin-right: -20px;
-    }
-  }
-}
-
-@keyframes fadeIn {
-    0% {
-        opacity: 0;
-    }
-    100% {
-        opacity: 1;
-    }
+.clearfix {
+  clear: both;
 }
 
 
-.offset-md-9 {
-  .chat-bubble {
-    background: $blue;
-    color: #fff;
-  }
+
+
+::-webkit-scrollbar-track
+{
+    box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,0.3);
+    background-color: #F5F5F5;
 }
 
-.chat-box-tray {
-  background: #eee;
-  display: flex;
-  align-items: baseline;
-  padding: 10px 15px;
-  align-items: center;
-  margin-top: 19px;
-  bottom: 0;
+::-webkit-scrollbar
+{
+    width: 12px;
+    background-color: #F5F5F5;
+}
 
-  input {
-    margin: 0 10px;
-    padding: 6px 2px;
-  }
-
-  i {
-    color: grey;
-    font-size: 30px;
-    vertical-align: middle;
-
-    &:last-of-type {
-      margin-left: 25px;
-    }
-  }
+::-webkit-scrollbar-thumb
+{
+    box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    -webkit-box-shadow: inset 0 0 6px rgba(0,0,0,.3);
+    background-color: #555;
 }
 </style>
+<br>
 <div class="container">
-	<div class="row no-gutters">
-	  <div class="col-md-4 border-right">
-		<div class="settings-tray">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/filip.jpg" alt="Profile img">
-		  <span class="settings-tray--right">
-			<i class="material-icons">cached</i>
-			<i class="material-icons">message</i>
-			<i class="material-icons">menu</i>
-		  </span>
-		</div>
-		<div class="search-box">
-		  <div class="input-wrapper">
-			<i class="material-icons">search</i>
-			<input placeholder="Search here" type="text">
-		  </div>
-		</div>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/robocop.jpg" alt="">
-		  <div class="text">
-			<h6>Robo Cop</h6>
-			<p class="text-muted">Hey, you're arrested!</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/optimus-prime.jpeg" alt="">
-		  <div class="text">
-			<h6>Optimus</h6>
-			<p class="text-muted">Wanna grab a beer?</p>
-		  </div>
-		  <span class="time text-muted small">00:32</span>
-		</div>
-		<hr>
-		<div class="friend-drawer friend-drawer--onhover ">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/real-terminator.png" alt="">
-		  <div class="text">
-			<h6>Skynet</h6>
-			<p class="text-muted">Seen that canned piece of s?</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/termy.jpg" alt="">
-		  <div class="text">
-			<h6>Termy</h6>
-			<p class="text-muted">Im studying spanish...</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/rick.jpg" alt="">
-		  <div class="text">
-			<h6>Richard</h6>
-			<p class="text-muted">I'm not sure...</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-		<hr>
-		<div class="friend-drawer friend-drawer--onhover">
-		  <img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/rachel.jpeg" alt="">
-		  <div class="text">
-			<h6>XXXXX</h6>
-			<p class="text-muted">Hi, wanna see something?</p>
-		  </div>
-		  <span class="time text-muted small">13:21</span>
-		</div>
-	  </div>
-	  <div class="col-md-8">
-		<div class="settings-tray">
-			<div class="friend-drawer no-gutters friend-drawer--grey">
-			<img class="profile-image" src="https://www.clarity-enhanced.net/wp-content/uploads/2020/06/robocop.jpg" alt="">
-			<div class="text">
-			  <h6>Robo Cop</h6>
-			  <p class="text-muted">Layin' down the law since like before Christ...</p>
-			</div>
-			<span class="settings-tray--right">
-			  <i class="material-icons">cached</i>
-			  <i class="material-icons">message</i>
-			  <i class="material-icons">menu</i>
-			</span>
-		  </div>
-		</div>
-		<div class="chat-panel">
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3">
-			  <div class="chat-bubble chat-bubble--left">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row no-gutters">
-			<div class="col-md-3 offset-md-9">
-			  <div class="chat-bubble chat-bubble--right">
-				Hello dude!
-			  </div>
-			</div>
-		  </div>
-		  <div class="row">
-			<div class="col-12">
-			  <div class="chat-box-tray">
-				<i class="material-icons">sentiment_very_satisfied</i>
-				<input type="text" placeholder="Type your message here...">
-				<i class="material-icons">mic</i>
-				<i class="material-icons">send</i>
-			  </div>
-			</div>
-		  </div>
-		</div>
-	  </div>
-	</div>
+  <div class="row">
+        <div class="col-md-6 mx-auto">
+            <div class="card">
+                <div class="card-header text-center">
+                    <span>Chat Box</span>
+                </div>
+                <div class="card-body chat-care">
+                    <ul class="chat">
+                        <li class="agent clearfix">
+                            <span class="chat-img left clearfix mx-2">
+                                <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="Agent" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>12 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="admin clearfix">
+                            <span class="chat-img right clearfix  mx-2">
+                                <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="Admin" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <small class="left text-muted"><span class="glyphicon glyphicon-time"></span>13 mins ago</small>
+                                    <strong class="right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor, quis ullamcorper ligula sodales.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="agent clearfix">
+                            <span class="chat-img left clearfix mx-2">
+                                <img src="http://placehold.it/50/55C1E7/fff&text=U" alt="Agent" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <strong class="primary-font">Jack Sparrow</strong> <small class="right text-muted">
+                                        <span class="glyphicon glyphicon-time"></span>14 mins ago</small>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet.
+                                </p>
+                            </div>
+                        </li>
+                        <li class="admin clearfix">
+                            <span class="chat-img right clearfix  mx-2">
+                                <img src="http://placehold.it/50/FA6F57/fff&text=ME" alt="Admin" class="img-circle" />
+                            </span>
+                            <div class="chat-body clearfix">
+                                <div class="header clearfix">
+                                    <small class="left text-muted"><span class="glyphicon glyphicon-time"></span>15 mins ago</small>
+                                    <strong class="right primary-font">Bhaumik Patel</strong>
+                                </div>
+                                <p>
+                                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur bibendum ornare
+                                    dolor.
+                                </p>
+                            </div>
+                        </li>
+                    </ul>
+                </div>
+                <div class="card-footer">
+                    <div class="input-group">
+                        <input id="btn-input" type="text" class="form-control input-sm" placeholder="Type your message here..." />
+                        <span class="input-group-btn">
+                            <button class="btn btn-primary" id="btn-chat">
+                                Send</button>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </div>
+@endsection
